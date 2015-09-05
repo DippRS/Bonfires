e4ebd492809d4f75cabbf1642cdaee77ea491e16
DippRS <cwwmbm@outlook.com>
2015-09-05 13:17:00 +0100

    First commit
    Just starting out

---
+++ src/org/dippers/bonfire/BonfireMain.java
@@ -0,0 +1,25 @@
+package org.dippers.bonfire;
+
+import org.powerbot.script.PollingScript;
+import org.powerbot.script.Script;
+import org.powerbot.script.rt6.ClientContext;
+
+@Script.Manifest(name="DippBonfire", description="Makes bonfires")
+public class BonfireMain extends PollingScript<ClientContext>
+{
+
+	@Override
+	public void start() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void poll() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
